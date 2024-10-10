4d4848a2ea553c776cc32cd0d1ff46b977390f14
JelCore <jelcore@icloud.com>
2024-10-10 16:33:13 +0800

    doc: update links.

--- src/manual_doc.ts
+++ src/manual_doc.ts
@@ -7,9 +7,9 @@ let helpDoc: {
 	helpText: `
 访问下方的帮助文档获取使用教程
 	`,
-	helpLink: "https://ydc.asia/docyun",
-	homeText: "欢迎访问易东云官网",
-	homeUrl: "https://www.ydc.show",
+	helpLink: "https://ydc.asia/doc_index",
+	homeText: "欢迎访问易东文档官网",
+	homeUrl: "https://web.ydc.show",
 };
 
 export default helpDoc;
